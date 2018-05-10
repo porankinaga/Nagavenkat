8c3ebc5ef3e6207def43bb34c7850268834ed4d9
Nagababu Poranki <nagababup@np040.eximiusdesign.in>
2018-05-10 13:48:16 +0530

    added toggle scenario

--- c/setbit.c
+++ c/setbit.c
@@ -1,14 +1,19 @@
+#include<stdio.h>
+
 main()
 {
-	int a = 10,x = 5,i;
+	int a = 10,x = 9,i;
 	printf("Before set bit %d\n",a);
-	for(i=0;i<=31;i++)
-		printf("%d bit value is %c\n",i,a&(1<<i)?'1':'0');
 	a = a|(1<<x);//set a bit
 	printf("After set bit %d\n",a);
 	a = a&~(1<<x);//clearing bit
 	printf("After clearing bit value is %d",a);
+	
+	a = a^(1<<x); //	Toggling a bit
+	for(i=0;i<=31;i++)
+		printf("%d bit value is %c\n",i,a&(1<<i)?'1':'0');
+ 	printf("After toggle value is %d",a);	
+	return 0;
 
 	
-	return 0;
 }
