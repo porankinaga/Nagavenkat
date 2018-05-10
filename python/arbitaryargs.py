07b9b6652981a9068afbda2d51664b70ce59ac65
Nagababu Poranki <nagababup@np040.eximiusdesign.in>
2018-05-10 12:24:41 +0530

    working python interview questions and preparing answers

---
+++ python/arbitaryargs.py
@@ -0,0 +1,6 @@
+def arbitry(*names):
+	for name in names:
+		print("Hello",name)
+
+arbitry("Naga","Steve","Sunderpichay","Zuckerburg")
+	
---
+++ python/cmdline.py
@@ -0,0 +1,6 @@
+import sys
+import os
+a = sys.argv[0]
+b = sys.argv[1]
+c = a+b
+print(c)
